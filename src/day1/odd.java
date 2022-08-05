05cc73c2a3d5955e0c48264c1bca372cd7a8f738
pralhad <pralhad.gamare@aqmtechnologies.com>
2022-08-05 17:36:28 +0530

    javacodecreated

---
+++ src/day1/odd.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class odd {
+
+	public static void main(String[] args) {
+		int a=5;
+		if(a%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("odd");
+
+	}
+
+}
